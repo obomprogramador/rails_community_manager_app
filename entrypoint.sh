87e5be9c4c2cb40b4652c9dfe24f9e2068d72e49
Carlos H.F.S <obomprogramador@gmail.com>
2026-05-05 18:32:16 -0300

    Feat: Changing the seed again.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 echo "Running migrations..."
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:drop db:create db:migrate
 
-echo "Precompiling assets..."
-bin/rails assets:precompile
-
 echo "Running seeds..."
 bin/rails db:seed
+
+echo "Precompiling assets..."
+bin/rails assets:precompile
 
 echo "Starting server..."
 exec bin/rails server -b 0.0.0.0 -p ${PORT:-3000}
