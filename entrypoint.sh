c0310ac441e10c3b26166eb0ca5bdc64a2fe0be5
Carlos H.F.S <obomprogramador@gmail.com>
2026-05-04 22:23:12 -0300

    Feat: Adjusting assets recompilation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Running migrations..."
 bin/rails db:migrate
 
+echo "Precompiling assets..."
+bin/rails assets:precompile
+
 echo "Starting server..."
 exec bin/rails server -b 0.0.0.0 -p ${PORT:-3000}
